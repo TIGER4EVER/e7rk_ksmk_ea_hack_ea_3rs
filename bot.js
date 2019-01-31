a92af578de79eedc4f66069fe8248504289bbe32
TIGER4EVER <47013568+TIGER4EVER@users.noreply.github.com>
2019-01-31 03:18:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NTM5Mzg1MDAwMTYwOTE5NTUz.DzBlCA.lL0hsKmF1DH9efvL_N4sUKThEr8";
+const discord_token = "NTQwMzM4NTQxNzA0NzczNjQy.DzPdKA.Z2cre7bjxEM23-KyvxrJd789yHs";
 var table = require('table').table
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
